2c0dfd79d8659f79c66324b6da9e45e219d9265d
zeprog <dmitrium02@mail.ru>
2020-05-29 04:00:33 +0300

    titles and text for types (pug)

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -6,6 +6,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     devtool: 'cheap-module-eval-source-map',
     devServer: {
         contentBase: baseWebpackConfig.externals.paths.dist,
+        liveServer: browser=Chrome,
         port: 8081,
         overlay: {
             warnings: true,
