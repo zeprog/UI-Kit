c450dd4d3cf930b94892bf32b445795f7d20d538
zeprog <dmitrium02@mail.ru>
2020-06-14 23:43:27 +0300

    Reconnected jQuery and connected mask input plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import $ from 'jquery';
+import './pages/pug/includes/inputs/jquery.maskedinput'
+
 
 
 import './pages/js/Cards'
 import './pages/js/Colors&type'
 import './pages/js/Headers&footers'
 import './pages/js/FormElements'
-import './pages/pug/includes/inputs/jquery.maskInput'
+
+$(function ($) {
+    $('.form__input_mask').mask('99.99.9999', { placeholder: "mm.dd.yyyy" });
+});
--- src/pages/js/FormElements.js
+++ src/pages/js/FormElements.js
@@ -1,2 +1,6 @@
-import '../scss/FormElements.scss'
-import '../pug/includes/inputs/jquery.maskInput'
+import '../scss/FormElements.scss';
+import '../pug/includes/inputs/jquery.maskInput';
+
+$(document).ready(function () {
+    $(".form__input_mask").mask("99.99.9999", { placeholder: "дд.мм.гггг" });
+})
