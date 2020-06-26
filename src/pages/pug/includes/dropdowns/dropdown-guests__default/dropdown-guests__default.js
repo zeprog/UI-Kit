1978b9b67a5b4b65c3dbb26ef4de21c8aed068be
zeprog <dmitrium02@mail.ru>
2020-06-26 18:08:34 +0300

    Count for dropdown

--- src/pages/pug/includes/dropdowns/dropdown-guests__default/dropdown-guests__default.js
+++ src/pages/pug/includes/dropdowns/dropdown-guests__default/dropdown-guests__default.js
@@ -1,8 +1,16 @@
 let placeholder = document.querySelector('.dropdown-guests__placeholder');
 let options = document.querySelector('.dropdown-guests__options');
 
+
 placeholder.onclick = function () {
     options.style.maxHeight = options.style.maxHeight == '131px' ? '0px' : '131px';
     options.style.opacity = options.style.opacity == '1' ? '0' : '1';
 };
 
+(function ($) {
+    $('.dropdown-guests__value').on('click', 'span', function () {
+        var inp = $('input', this.parentNode),
+            val = +inp.val() || 0;
+        inp.val(val += this.className == 'value_down' ? val > 0 ? -1 : 0 : 1);
+    });
+}(jQuery));
